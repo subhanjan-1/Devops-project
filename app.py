5cddd3c31e7a5b08d07f4cd52b4ecad1ef066952
Subhanjan Saha <subhanjansaha2003@gmail.com>
2026-04-15 13:10:14 +0530

    fixed runtime error

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def home():
         except Exception as e:
             prediction_text = f"Error: {e}"
 
-    return render_template('index.htm', prediction_text=prediction_text)
+    return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == '__main__':
     app.run(debug=True)
